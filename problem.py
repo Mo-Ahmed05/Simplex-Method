8fb4bc7cd699c520ff2cb9b1568c489759127ded
mo <mhahib5@gmail.com>
2025-11-16 17:18:04 +0200

    Update example objective function and constraints in problem.py

--- problem.py
+++ problem.py
@@ -5,9 +5,9 @@ from simplex_method import simplex_method as sm
 # Objective Function: 12X_1 + 8X_2              --> [12, 8]
 # S.T. (Solve To):  5X_1 + 2X_2 <= 150          --> [[5, 2, '<=', 150]]
 
-objective_func = [12, 8]
-st = [[5, 2, '<=', 150], 
-      [2, 3, '<=', 100],
-      [4, 2, '<=', 80]]
+objective_func = [3, 2, 5]
+st = [[1, 2, 1, '<=', 43], 
+      [3, 0, 2, '<=', 46],
+      [1, 4, 0, '<=', 42]]
 
 sm(objective_func, st, max=True)
